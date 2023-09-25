e46a19a07a3d70b552734c9fe16e2c93711b3c10
Bryan Jones <thebeard@me.com>
2023-09-25 12:41:37 -0700

    added dependencies

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
+import { Fragment } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { theme } from './src/infrastructure/theme';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Fragment>
+      <StatusBar style='auto' />
+    </Fragment>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
